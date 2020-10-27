7d03b2ea63e2c22d1544ebe1987942655fc9e6cc
Martin Mucha <martin.mucha@embedit.cz>
2020-10-27 13:24:46 +0100

    removed extra todo

--- src/main/java/com/gmail/alfonz19/util/initialize/Config.java
+++ src/main/java/com/gmail/alfonz19/util/initialize/Config.java
@@ -1,8 +1,7 @@
 package com.gmail.alfonz19.util.initialize;
 
+//TODO MMUCHA: externalize
 public class Config {
-    //TODO MMUCHA: externalize
     public static final int MAX_STRING_LENGTH = 100_000;
-    //TODO MMUCHA: externalize
     public static final int UNCONFIGURED_STRING_SIZE = 20;
 }
