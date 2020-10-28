df5aa831fddb2fa50628ed5c9d11a2dba3016909
Martin Mucha <martin.mucha@embedit.cz>
2020-10-28 10:39:45 +0100

    renamed tests, removed todo

--- src/test/java/com/gmail/alfonz19/util/initialize/context/PathMatcherBuilderTest.java
+++ src/test/java/com/gmail/alfonz19/util/initialize/context/PathMatcherBuilderTest.java
@@ -2,179 +2,176 @@ package com.gmail.alfonz19.util.initialize.context;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.regex.Pattern;
-
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-//TODO MMUCHA: rename.
 @Slf4j
 public class PathMatcherBuilderTest {
 
     @Test
-    public void t1() {
+    public void testMatchingRootPath() {
         assertPattern(PathBuilder.root().getPath(),
                 PathMatcherBuilder.root().build(),
                 true);
     }
 
     @Test
-    public void t2() {
+    public void testIncorrectPath1() {
         assertPattern("..",
                 PathMatcherBuilder.root().build(),
                 false);
     }
 
     @Test
-    public void t3() {
+    public void testIncorrectPath2() {
         assertPattern(PathBuilder.root().addProperty("abc").getPath(),
                 PathMatcherBuilder.root().build(),
                 false);
     }
 
     @Test
-    public void t4() {
+    public void testMatchingSpecificArrayIndex() {
         assertPattern(PathBuilder.root().addArrayIndex(1).getPath(),
                 PathMatcherBuilder.root().addArrayIndex(1).build(),
                 true);
     }
 
     @Test
-    public void t5() {
+    public void testNotMatchingDifferentIndex() {
         assertPattern(PathBuilder.root().addArrayIndex(1).getPath(),
                 PathMatcherBuilder.root().addArrayIndex(2).build(),
                 false);
     }
 
     @Test
-    public void t6() {
+    public void testNotMatchingAssociativeArrayKeyByArrayIndex() {
         assertPattern(PathBuilder.root().addAssociativeArrayKey("1").getPath(),
                 PathMatcherBuilder.root().addArrayIndex(1).build(),
                 false);
     }
 
     @Test
-    public void t7() {
+    public void testMatchingAssociativeArrayKey() {
         assertPattern(PathBuilder.root().addAssociativeArrayKey("1").getPath(),
                 PathMatcherBuilder.root().addAssociativeArrayKey("1").build(),
                 true);
     }
 
     @Test
-    public void t8() {
+    public void testNotMatchingArrayIndexByAssociativeArrayKey() {
         assertPattern(PathBuilder.root().addArrayIndex(1).getPath(),
                 PathMatcherBuilder.root().addAssociativeArrayKey("1").build(),
                 false);
     }
 
     @Test
-    public void t9() {
+    public void testMatchingProperty() {
         assertPattern(PathBuilder.root().addProperty("abc").getPath(),
                 PathMatcherBuilder.root().addProperty("abc").build(),
                 true);
     }
 
     @Test
-    public void t10() {
+    public void testNotMatchingProperty() {
         assertPattern(PathBuilder.root().addProperty("abcde").getPath(),
                 PathMatcherBuilder.root().addProperty("abc").build(),
                 false);
     }
 
     @Test
-    public void t11() {
+    public void testMatchingPathHavingTwoProperties() {
         assertPattern(PathBuilder.root().addProperty("abc").addProperty("def").getPath(),
                 PathMatcherBuilder.root().addProperty("abc").addProperty("def").build(),
                 true);
     }
 
     @Test
-    public void t12() {
+    public void testMatchingPathHavingPropertyAndArrayIndex() {
         assertPattern(PathBuilder.root().addProperty("abc").addArrayIndex(1).getPath(),
                 PathMatcherBuilder.root().addProperty("abc").addArrayIndex(1).build(),
                 true);
     }
 
     @Test
-    public void t13() {
+    public void testMatchingPathHavingPropertyAndAssociativeArrayKey() {
         assertPattern(PathBuilder.root().addProperty("abc").addAssociativeArrayKey("1").getPath(),
                 PathMatcherBuilder.root().addProperty("abc").addAssociativeArrayKey("1").build(),
                 true);
     }
 
     @Test
-    public void t14() {
+    public void testMatchingPathHavingPropertyArrayIndexAndProperty() {
         assertPattern(PathBuilder.root().addProperty("abc").addArrayIndex(1).addProperty("def").getPath(),
                 PathMatcherBuilder.root().addProperty("abc").addArrayIndex(1).addProperty("def").build(),
                 true);
     }
 
     @Test
-    public void t15() {
+    public void testMatchingAnyPropertyAtPathStart() {
         assertPattern(PathBuilder.root().addProperty("abc").addArrayIndex(1).addProperty("def").getPath(),
                 PathMatcherBuilder.root().addAnyProperty().addArrayIndex(1).addProperty("def").build(),
                 true);
     }
 
     @Test
-    public void t15_2() {
+    public void testMatchingOptionalPropertyWhenOneIsThereToBeMatched() {
         assertPattern(PathBuilder.root().addProperty("abc").addArrayIndex(1).addProperty("def").getPath(),
                 PathMatcherBuilder.root().addOptionalProperty().addArrayIndex(1).addProperty("def").build(),
                 true);
     }
 
     @Test
-    public void t15_3() {
+    public void testMatchingOptionalPropertyWhenThereIsntOneToBeMatched() {
         assertPattern(PathBuilder.root().addArrayIndex(1).addProperty("def").getPath(),
                 PathMatcherBuilder.root().addOptionalProperty().addArrayIndex(1).addProperty("def").build(),
                 true);
     }
 
     @Test
-    public void t16() {
+    public void testMatchingAnyPropertyAtPathEnd() {
         assertPattern(PathBuilder.root().addProperty("abc").addArrayIndex(1).addProperty("def").getPath(),
                 PathMatcherBuilder.root().addProperty("abc").addArrayIndex(1).addAnyProperty().build(),
                 true);
     }
 
     @Test
-    public void t17() {
+    public void testMatchingAnyArrayIndexAndAnyProperty() {
         assertPattern(PathBuilder.root().addProperty("abc").addArrayIndex(1).addProperty("def").getPath(),
                 PathMatcherBuilder.root().addProperty("abc").addAnyArrayIndex().addAnyProperty().build(),
                 true);
     }
 
     @Test
-    public void t18() {
+    public void testMatchingAnyPathComponentFollowedByAnyIndexAndAnyProperty() {
         assertPattern(PathBuilder.root().addProperty("abc").addArrayIndex(1).addProperty("def").getPath(),
                 PathMatcherBuilder.root().addAnyPathComponent().addAnyArrayIndex().addAnyProperty().build(),
                 true);
     }
 
     @Test
-    public void t19() {
+    public void testMatchingAnyPathComponentToMatchProperty() {
         assertPattern(PathBuilder.root().addProperty("abc").getPath(),
                 PathMatcherBuilder.root().addAnyPathComponent().build(),
                 true);
     }
 
     @Test
-    public void t20() {
+    public void testMatchingAnyPathComponentToMatchArrayIndex() {
         assertPattern(PathBuilder.root().addArrayIndex(0).getPath(),
                 PathMatcherBuilder.root().addAnyPathComponent().build(),
                 true);
     }
 
     @Test
-    public void t21() {
+    public void testMatchingAnyPathComponentToMatchAssociativeArrayKey() {
         assertPattern(PathBuilder.root().addAssociativeArrayKey("0").getPath(),
                 PathMatcherBuilder.root().addAnyPathComponent().build(),
                 true);
     }
 
     @Test
-    public void t22() {
+    public void testMatchingMultipleUsageOfAnyPathComponent() {
         assertPattern(PathBuilder.root().addProperty("abc").addArrayIndex(1).addProperty("def").getPath(),
                 PathMatcherBuilder.root()
                         .addAnyPathComponent()
@@ -185,93 +182,77 @@ public class PathMatcherBuilderTest {
     }
 
     @Test
-    public void t23() {
+    public void testMatchingPathUsingZeroToMultipleMatcherMatchingAnySubPath1() {
         assertPattern(PathBuilder.root().addProperty("abc").addArrayIndex(1).addProperty("def").getPath(),
                 PathMatcherBuilder.root().addAnySubPath().addAnyPathComponent().addAnyPathComponent().build(),
                 true);
     }
 
     @Test
-    public void t24() {
+    public void testMatchingPathUsingZeroToMultipleMatcherMatchingAnySubPath2() {
         assertPattern(PathBuilder.root().addProperty("abc").addArrayIndex(1).addProperty("def").getPath(),
                 PathMatcherBuilder.root().addAnySubPath().addAnyPathComponent().build(),
                 true);
     }
 
     @Test
-    public void t25() {
+    public void testMatchingPathUsingZeroToMultipleMatcherMatchingAnySubPath3() {
         assertPattern(PathBuilder.root().addProperty("abc").addArrayIndex(1).addProperty("def").getPath(),
                 PathMatcherBuilder.root().addAnySubPath().build(),
                 true);
     }
 
     @Test
-    public void t26() {
+    public void testMatchingPathUsingZeroToMultipleMatcherMatchingAnySubPath4() {
         assertPattern(PathBuilder.root().addProperty("whatever").getPath(),
                 PathMatcherBuilder.root().addAnySubPath().addAnyProperty().build(),
                 true);
     }
 
-    //-------------------
-
     @Test
-    public void t27() {
+    public void testMatchingOptionalAnyPathComponentToMatchProperty() {
         assertPattern(PathBuilder.root().addProperty("abc").addArrayIndex(1).addProperty("def").getPath(),
                 PathMatcherBuilder.root().addAnyOptionalPathComponent().addAnyArrayIndex().addAnyProperty().build(),
                 true);
     }
 
     @Test
-    public void t27_2() {
+    public void testMatchingOptionalAnyPathComponentToMatchArrayIndex() {
         assertPattern(PathBuilder.root().addArrayIndex(1).getPath(),
                 PathMatcherBuilder.root().addAnyOptionalPathComponent().addAnyArrayIndex().addAnyOptionalPathComponent().build(),
                 true);
     }
 
     @Test
-    public void t28() {
+    public void testMatchingOptionalAnyPathComponentToMatchProperty2() {
         assertPattern(PathBuilder.root().addProperty("abc").getPath(),
                 PathMatcherBuilder.root().addAnyOptionalPathComponent().build(),
                 true);
     }
 
     @Test
-    public void t28_2() {
+    public void testMatchingOptionalAnyPathComponentToNothing() {
         assertPattern(PathBuilder.root().getPath(),
                 PathMatcherBuilder.root().addAnyOptionalPathComponent().build(),
                 true);
     }
 
     @Test
-    public void t29() {
+    public void testMatchingOptionalAnyPathComponentToMatchArrayIndex2() {
         assertPattern(PathBuilder.root().addArrayIndex(0).getPath(),
                 PathMatcherBuilder.root().addAnyOptionalPathComponent().build(),
                 true);
     }
 
     @Test
-    public void t29_2() {
-        assertPattern(PathBuilder.root().getPath(),
-                PathMatcherBuilder.root().addAnyOptionalPathComponent().build(),
-                true);
-    }
-
-    @Test
-    public void t30() {
+    public void testMatchingOptionalAnyPathComponentToMatchAssociativeArrayKey() {
         assertPattern(PathBuilder.root().addAssociativeArrayKey("0").getPath(),
                 PathMatcherBuilder.root().addAnyOptionalPathComponent().build(),
                 true);
     }
 
     @Test
-    public void t30_2() {
-        assertPattern(PathBuilder.root().getPath(),
-                PathMatcherBuilder.root().addAnyOptionalPathComponent().build(),
-                true);
-    }
-
-    @Test
-    public void t31() {
+    public void testMatchingOptionalAnyPathComponent() {
         assertPattern(PathBuilder.root().addProperty("abc").addArrayIndex(1).addProperty("def").getPath(),
                 PathMatcherBuilder.root()
                         .addAnyOptionalPathComponent()
@@ -282,7 +263,7 @@ public class PathMatcherBuilderTest {
     }
 
     @Test
-    public void t31_2() {
+    public void testMatchingOptionalAnyPathComponentToNothing3() {
         assertPattern(PathBuilder.root().getPath(),
                 PathMatcherBuilder.root()
                         .addAnyOptionalPathComponent()
